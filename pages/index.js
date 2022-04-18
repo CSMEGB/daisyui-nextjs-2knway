01d1b8552f9f4bf410382d3ed97ef373290b9dbf
CSMEGB <digital.platform@csmegb.net>
2022-04-18 14:30:18 +0100

    2 files modified, 3 added

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,16 @@
 import Head from 'next/head';
-import NavBar from './components/NavBar.js'
+import NavBar from './components/NavBar.js';
+import Testimonial from './components/Testimonial.js';
+import Book from './components/Book.js';
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Create Next App</title>
       </Head>
-      <div class="navbar bg-base-100">
-  <div class="flex-1">
-    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <NavBar></NavBar>
-</div>
-      <div>
-        <button className="btn">Hello daisyUI</button>
-        <button class="btn">Button Test</button>
-        <button class="btn btn-primary">Button</button>
-        <button class="btn btn-secondary">Button</button>
-        <button class="btn btn-accent">Button</button>
-        <button class="btn btn-ghost">Button</button>
-        <button class="btn btn-link">Button</button>
-      </div>
+      <NavBar></NavBar>
+      <Testimonial></Testimonial>
+      <Book></Book>
     </div>
   );
 }
